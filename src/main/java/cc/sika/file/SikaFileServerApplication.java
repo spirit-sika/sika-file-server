ab4e1ce19d6b99ec323827ab96f5695552a0365a
小吴来哩 <wuchang@gdcattsoft.com>
2025-08-01 20:33:17 +0800

    init

---
+++ src/main/java/cc/sika/file/SikaFileServerApplication.java
@@ -0,0 +1,13 @@
+package cc.sika.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SikaFileServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SikaFileServerApplication.class, args);
+	}
+
+}
